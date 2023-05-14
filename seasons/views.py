92a44450e59b948b7e18d77aae3d0e135df82b64
ismaelSS <ismael1337sn@gmail.com>
2023-05-13 23:31:50 -0300

    feat: create app seasons X finishing

--- seasons/views.py
+++ seasons/views.py
@@ -1,12 +1,20 @@
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from utils.permissions import IsAccountOwnerAndPathOrAcconuntOwnerOrAdmin
 from .models import Season
 from .serializers import SeasonSerializer
 
-class UserView(generics.ListCreateAPIView):
+class SeasonsView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    queryset = Season.objects.all()
+    serializer_class = SeasonSerializer
+
+class SeasonDetailViews(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerAndPathOrAcconuntOwnerOrAdmin]
 
     queryset = Season.objects.all()
     serializer_class = SeasonSerializer
